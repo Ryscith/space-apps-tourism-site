df6ae2680de7b1ea79af0b45ad3ec890fe38b9e0
Meghana Madiraju <meghanambpd@gmail.com>
2023-10-08 11:00:56 -0400

    Commit message

--- main.go
+++ main.go
@@ -7,16 +7,6 @@ import (
 	"github.com/gofiber/template/django/v3"
 )
 
-type Question struct {
-	Text    string
-	Choices []Choice
-}
-
-type Choice struct {
-	Text string
-	Id   int
-}
-
 func main() {
 	// Create a new engine
 	engine := django.New("./views", ".django")
@@ -52,35 +42,12 @@ func main() {
 
 	app.Get("/questionnaire", func(c *fiber.Ctx) error {
 		// Render index within layouts/main
-		return c.Render("pages/questionnaire", fiber.Map{
-			"questions": []Question{
-				{
-					"What temperature do you prefer?",
-					[]Choice{{"Hot", 0}, {"Temperate", 1}, {"Cold", 2}, {"No Preference", 3}},
-				},
-				{
-					"Do you like geology?",
-					[]Choice{{"I love it!", 0}, {"I hate it.", 1}, {"No Preference", 2}},
-				},
-				{
-					"How much time do you have?",
-					[]Choice{{"Less than a week", 0}, {"A week", 1}, {"More than a week", 2}},
-				},
-				{
-					"Do you have any heart problems?",
-					[]Choice{{"Yes", 0}, {"No", 1}},
-				},
-				{
-					"Which would you prefer?",
-					[]Choice{{"Sight seeing from space", 0}, {"Seeing a planet from it's surface", 1}},
-				},
-			},
-		})
+		return c.Render("pages/questionnaire", fiber.Map{})
 	})
 
-	app.Post("/questionnaire", func(c *fiber.Ctx) error {
-		log.Print(c.Request())
-		return c.Render("pages/homepage", fiber.Map{})
+	app.Get("/destination", func(c *fiber.Ctx) error {
+		// Render index within layouts/main
+		return c.Render("pages/destination", fiber.Map{})
 	})
 
 	log.Fatal(app.Listen(":3000"))
